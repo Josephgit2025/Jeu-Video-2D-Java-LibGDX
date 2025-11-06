9838398fc51fae681c669f7370f16dbb3c4014c0
dcyprien <dcyprien@42.student.fr>
2025-11-06 15:12:11 +0100

    merge

--- core/src/main/java/com/main/entities/enemies/Zombie.java
+++ core/src/main/java/com/main/entities/enemies/Zombie.java
@@ -1,22 +1,3 @@
-<<<<<<< HEAD:game/src/main/java/com/main/entities/enemies/Zombie.java
-package com.main.entities.enemies;
-
-import com.main.entities.Unit;
-
-public class Zombie extends Unit {
-
-    public Zombie (String filePath, int posX, int posY) {
-        super(filePath, posX, posY);
-        this.health = 100;
-        this.attackDamage = 15;
-        this.speed = 2;
-        this.attackSpeed = 1;
-        this.range = 100;
-    }
-
-}
-    
-=======
 package com.main.entities.enemies;
 
 import com.main.entities.Unit;
@@ -33,4 +14,3 @@ public class Zombie extends Unit {
     }
 
 }
->>>>>>> feature/migration:core/src/main/java/com/main/entities/enemies/Zombie.java
